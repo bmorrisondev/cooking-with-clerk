a3271ac80ff26f9940f1ce0186aa727b3fe84de2
Nico Albanese <nico.albanese@vercel.com>
2024-05-28 21:16:36 +0100

    update ai sdk implementation

---
+++ src/app/app/stream-object/page.tsx
@@ -0,0 +1,82 @@
+"use client";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+import { useState } from "react";
+import LoadingSpinner from "@/components/LoadingSpinner";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { PartialRecipes } from "@/models/recipes";
+import { generateRecipes } from "./actions";
+import { readStreamableValue } from "ai/rsc";
+
+export default function Home() {
+  const [recipes, setRecipes] = useState<PartialRecipes>();
+  const [isLoading, setIsLoading] = useState(false);
+  const [input, setInput] = useState<string>("");
+
+  return (
+    <main>
+      <form
+        onSubmit={async (e) => {
+          e.preventDefault();
+          setIsLoading(true);
+          const { recipes: stream } = await generateRecipes(input);
+          for await (const partialRecipes of readStreamableValue(stream)) {
+            setIsLoading(false);
+            setRecipes(partialRecipes);
+          }
+        }}
+        className="flex items-center gap-2 mb-4"
+      >
+        <Input
+          type="text"
+          placeholder="Specify some themes or ingredients"
+          value={input}
+          onChange={(e) => setInput(e.target.value)}
+        />
+        <Button type="submit">Generate ideas!</Button>
+      </form>
+      {isLoading && <LoadingSpinner />}
+      {!isLoading && (
+        <div className="grid md:grid-cols-3 gap-4">
+          {recipes?.recipes
+            ? recipes.recipes.length > 0 &&
+              recipes?.recipes?.map((recipe, i) => (
+                <Card key={i} className="flex flex-col flex-1">
+                  <CardHeader>
+                    <CardTitle>{recipe?.name}</CardTitle>
+                    <CardDescription>{recipe?.description}</CardDescription>
+                  </CardHeader>
+                  <CardContent className="flex flex-col flex-1">
+                    <div className="flex flex-col flex-1 mb-2">
+                      <div>Ingredients:</div>
+                      <div className="border shadow-sm rounded mb-2">
+                        <ul className="text-sm list-disc ml-4 p-2">
+                          {recipe?.ingredients?.map((ingredient, i: number) => (
+                            <li key={i}>
+                              {ingredient?.name} ({ingredient?.amount})
+                            </li>
+                          ))}
+                        </ul>
+                      </div>
+                      <ol className="list-decimal ml-4">
+                        {recipe?.instructions?.map((step, i: number) => (
+                          <li key={i}>{step}</li>
+                        ))}
+                      </ol>
+                    </div>
+                    {/* <Button onClick={() => alert("You clicked the button!")}>Save</Button> */}
+                  </CardContent>
+                </Card>
+              ))
+            : null}
+        </div>
+      )}
+    </main>
+  );
+}
---
+++ src/app/app/stream-ui/actions.tsx
@@ -0,0 +1,25 @@
+"use server";
+import { recipesSchema } from "@/models/recipes";
+import { openai } from "@ai-sdk/openai";
+import { streamObject } from "ai";
+import { createStreamableUI } from "ai/rsc";
+import { Recipes } from "./_components/recipes";
+import LoadingSpinner from "@/components/LoadingSpinner";
+
+export async function generateRecipes(input: string) {
+  "use server";
+  const recipesStream = createStreamableUI(<LoadingSpinner />);
+
+  (async () => {
+    const result = await streamObject({
+      model: openai("gpt-3.5-turbo"),
+      schema: recipesSchema,
+      prompt: `Generate recipes with the following ingredients or themes: ${input}`,
+    });
+    for await (const partialRecipes of result.partialObjectStream) {
+      recipesStream.update(<Recipes recipes={partialRecipes} />);
+    }
+    recipesStream.done();
+  })();
+  return { recipes: recipesStream.value };
+}
