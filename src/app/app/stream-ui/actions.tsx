ba978536db00227b6900280a85f151fe24eefb5c
Nico Albanese <nico.albanese@vercel.com>
2024-05-29 10:41:30 +0100

    add streamui and migrate existing streamui to streamable-ui

--- src/app/app/stream-ui/actions.tsx
+++ src/app/app/stream-ui/actions.tsx
@@ -1,25 +1,36 @@
 "use server";
 import { recipesSchema } from "@/models/recipes";
 import { openai } from "@ai-sdk/openai";
-import { streamObject } from "ai";
-import { createStreamableUI } from "ai/rsc";
+import { generateObject } from "ai";
+import { streamUI } from "ai/rsc";
 import { Recipes } from "./_components/recipes";
 import LoadingSpinner from "@/components/LoadingSpinner";
+import { z } from "zod";
 
 export async function generateRecipes(input: string) {
   "use server";
-  const recipesStream = createStreamableUI(<LoadingSpinner />);
 
-  (async () => {
-    const result = await streamObject({
-      model: openai("gpt-3.5-turbo"),
-      schema: recipesSchema,
-      prompt: `Generate recipes with the following ingredients or themes: ${input}`,
-    });
-    for await (const partialRecipes of result.partialObjectStream) {
-      recipesStream.update(<Recipes recipes={partialRecipes} />);
-    }
-    recipesStream.done();
-  })();
-  return { recipes: recipesStream.value };
+  const result = await streamUI({
+    model: openai("gpt-3.5-turbo"),
+    tools: {
+      generateRecipes: {
+        description:
+          "Generate recipes based on the ingredients or theme the user gives you.",
+        parameters: z.object({ ingredients: z.array(z.string()) }),
+        generate: async function* ({ ingredients }) {
+          yield <LoadingSpinner />;
+
+          const recipesGeneration = await generateObject({
+            schema: recipesSchema,
+            prompt: `Generate recipes with the following ingredients or themes: ${ingredients.join(", ")}`,
+            model: openai("gpt-3.5-turbo"),
+          });
+
+          return <Recipes recipes={recipesGeneration.object} />;
+        },
+      },
+    },
+    prompt: `Generate recipes with the following ingredients or themes: ${input}`,
+  });
+  return { recipes: result.value };
 }
